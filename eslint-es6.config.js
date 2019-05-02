a8699a25dd5050b67b7007813bcf754629325e55
Edie Lemoine <edie@myparcel.nl>
2019-05-02 11:43:13 +0200

    MY-11530 merge es6 stuff into base config

--- eslint-es6.config.js
+++ eslint-es6.config.js
@@ -1,28 +1,8 @@
-const {defaultParserOptions} = require('./exports');
-
 module.exports = {
-  ...defaultParserOptions,
   env: {
     es6: true,
   },
   extends: [
     './eslint.config.js',
-    './eslint-babel.config.js',
   ],
-  rules: {
-    'arrow-parens': 'warn',
-    'arrow-spacing': ['warn', {'before': true, 'after': true}],
-    'no-confusing-arrow': 'warn',
-    'no-const-assign': 'warn',
-    'no-dupe-class-members': 'warn',
-    'no-duplicate-imports': 'warn',
-    'no-useless-rename': 'warn',
-    'no-var': 'warn',
-    'prefer-const': 'warn',
-    'prefer-destructuring': ['warn', {'array': false, 'object': true}],
-    'prefer-spread': 'warn',
-    'prefer-template': 'warn',
-    'sort-imports': 'warn',
-    'template-curly-spacing': 'warn',
-  },
 };
