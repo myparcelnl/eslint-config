45014e7404420fc2c7e2ace21f217701a1550b06
Edie Lemoine <edie@myparcel.nl>
2019-11-21 11:31:26 +0100

    dev: fix version and release configurations

--- private/release.js
+++ private/release.js
@@ -7,7 +7,7 @@ if (args.includes('--dry-run')) {
   githubReleaseArgs.push('--dryRun');
 }
 
-const standardVersion = spawn('standard-version', ['-t', ...standardVersionArgs, ...args], {stdio: 'inherit'});
+const standardVersion = spawn('standard-version', [...standardVersionArgs, ...args], {stdio: 'inherit'});
 
 standardVersion.on('close', () => {
   spawn('github-release-from-changelog', [...githubReleaseArgs], {stdio: 'inherit'});
