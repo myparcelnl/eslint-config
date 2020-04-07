88a6c326662f2a7f189e88e73533feceeb1add1c
Edie Lemoine <edie@myparcel.nl>
2020-04-07 15:39:32 +0200

    build: fix husky hooks

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   // Don't forget to run ESLint on your ESLint
-  'precommit': 'npm run lint',
-  'prepush': 'npm run test',
+  'pre-commit': 'npm run lint',
+  'pre-push': 'npm run test',
 };
