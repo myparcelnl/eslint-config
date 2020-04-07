199b475b76a059389c337a229908f62ba04848b7
Edie Lemoine <edie@myparcel.nl>
2020-04-07 12:55:41 +0200

    build: run test on prepush hook

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,9 +1,6 @@
-const preCommit = [
-  // Don't forget to run ESLint on your ESLint
-  'npm run lint',
-];
-
 module.exports = {
   'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-  'precommit': preCommit.join(' && '),
+  // Don't forget to run ESLint on your ESLint
+  'precommit': 'npm run lint',
+  'prepush': 'npm run test',
 };
