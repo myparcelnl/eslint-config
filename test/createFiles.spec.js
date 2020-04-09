04d34191463f85e6efe3222ef005cfb81ba0060d
Edie Lemoine <edie@myparcel.nl>
2020-04-09 11:51:32 +0200

    test: move mock

--- test/createFiles.spec.js
+++ test/createFiles.spec.js
@@ -3,25 +3,25 @@ const fs = require('fs');
 const {baseDir} = require('../src/baseDir');
 
 describe('createFiles', () => {
-  /**
-   * Mock fs so the test will not actually create and write to files and suppress console statements.
-   */
-  beforeEach(() => {
+  test('Script runs without errors', async() => {
+    /**
+     * Mock fs so the test will not actually create and write to files and suppress console statements.
+     */
     jest.spyOn(fs, 'existsSync').mockImplementation(() => true);
-
     [
       jest.spyOn(console, 'log'),
       jest.spyOn(fs, 'writeFileSync'),
     ].forEach((spy) => {
       spy.mockImplementation(jest.fn());
     });
-  });
 
-  test('Script runs without errors', async() => {
     expect.assertions(1);
     const promise = import('../src/createFiles');
 
     await expect(promise).resolves.toBeTruthy();
+
+    jest.resetAllMocks();
+    jest.restoreAllMocks();
   });
 
   test('No absolute paths are used in the configs', async() => {
@@ -29,9 +29,5 @@ describe('createFiles', () => {
     const baseFile = (await import(path.resolve(baseDir, 'preset-default.js'))).default;
 
     expect(baseFile.extends.every((item) => !item.startsWith('/'))).toBe(true);
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
   });
 });
