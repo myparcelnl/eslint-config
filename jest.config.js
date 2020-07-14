d5cc303f5f2b0ab83154a5b99de376a4c5dc2392
Edie Lemoine <edie@myparcel.nl>
2020-07-14 12:15:20 +0200

    chore!: update packages (dropped support for node.js 8)
    - eslint 6.8.0 -> 7.4.0
    - @typescript-eslint 2.27.0 -> 3.6.1 (dropped support for typescript 3.2)

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,26 @@
 module.exports = {
   'clearMocks': true,
-  'collectCoverageFrom': ['<rootDir>/src/**/*.*'],
+  'collectCoverageFrom': [
+    '<rootDir>/src/**/*.js',
+    '<rootDir>/src/**/*.ts',
+  ],
   'transform': {
     '^.+\\.[t|j]sx?$': 'babel-jest',
   },
   'projects': [
     {
       displayName: 'test',
-      testMatch: ['<rootDir>/test/**/*.spec.*'],
+      testMatch: [
+        '<rootDir>/test/**/*.spec.js',
+      ],
     },
     {
       displayName: 'lint',
       runner: 'jest-runner-eslint',
-      testMatch: ['<rootDir>/test/**/*.lint.*'],
+      testMatch: [
+        '<rootDir>/test/**/*.lint.js',
+        '<rootDir>/test/**/*.lint.ts',
+      ],
     },
   ],
 };
