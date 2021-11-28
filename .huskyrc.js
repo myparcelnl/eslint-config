89993b8268b0cd90c5b9cc3fc1ada18df59fa4b1
Edie Lemoine <edie@myparcel.nl>
2021-11-28 14:45:10 +0100

    build: replace main branch

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   hooks: {
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     // Don't forget to run ESLint on your ESLint
     'pre-commit': 'npm run lint',
     'pre-push': 'npm test',
