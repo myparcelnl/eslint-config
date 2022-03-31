9bf24daaf1614b9a8fd5f6d874421534a6a3d7d1
Edie Lemoine <edie@myparcel.nl>
2022-03-31 10:02:27 +0200

    chore: update release config

---
+++ CONTRIBUTING.md
@@ -0,0 +1,15 @@
+# Contributing
+
+1. Make your changes
+2. Commit according to the following rules:
+    - `new` If you added one or more new rules or configurations
+    - `rule` If you changed one or more new rules that were already implemented. Keep it contained to one plugin/preset, and include the name in the scope. Example: `rule(jest): disabled jest/no-hooks` 
+    - If you've introduced breaking changes, you need to add one of the following to
+      your commit body:
+      `BREAKING CHANGE: ...`
+      `BREAKING CHANGES: ...`
+    - Other valid types:
+      - `chore`: Updating dependencies, changing ci configs, etc.
+      - `docs`: Documentation changes
+      - `build`: Changes in the build process of this module itself
+4. Create a pull request!
--- release.config.js
+++ release.config.js
@@ -1,12 +1,34 @@
 const {addGitHubPlugin} = require('@myparcel/semantic-release-config/src/plugins/addGitHubPlugin');
 const mainConfig = require('@myparcel/semantic-release-config');
-const npmConfig = require('@myparcel/semantic-release-config/npm');
+const {addCommitAnalyzerPlugin, addReleaseNotesGeneratorPlugin, addChangelogPlugin, addNpmPlugin} = require(
+  '@myparcel/semantic-release-config/src/plugins',
+);
 
 module.exports = {
   ...mainConfig,
   'extends': '@myparcel/semantic-release-config/npm',
   'plugins': [
-    ...npmConfig.plugins,
+    addCommitAnalyzerPlugin({
+      'releaseRules': [
+        {'type': 'breaking', 'release': 'major'},
+        {'type': 'new', 'release': 'minor'},
+        {'type': 'rule', 'release': 'patch'},
+      ],
+      'parserOpts': {
+        'noteKeywords': ['BREAKING CHANGE', 'BREAKING CHANGES'],
+      },
+    }),
+    addReleaseNotesGeneratorPlugin({
+      'presetConfig': {
+        types: [
+          {'type': 'breaking', 'section': 'Breaking 💥', 'hidden': false},
+          {'type': 'new', 'section': 'New ✨', 'hidden': false},
+          {'type': 'rule', 'section': 'Rule changes 📎', 'hidden': false},
+        ],
+      },
+    }),
+    addChangelogPlugin(),
+    addNpmPlugin(),
     addGitHubPlugin(),
   ],
 };
